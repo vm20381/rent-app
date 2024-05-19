91e557f1e1bde9ee9db37e1d00e55d8c0f61cd48
Chris-C3A <anon_c3a@outlook.com>
2024-05-19 18:23:57 +0100

    feat(chris): Created Todos page using custom widgets and displays realtime todos from the firestore collection

---
+++ apps/crew/chris_c3a/lib/views/todos/todos_page.dart
@@ -0,0 +1,42 @@
+import 'package:chris_c3a/models/todo_model.dart';
+import 'package:chris_c3a/widgets/todos/todos_list.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '/views/layouts/layout.dart';
+import '../../controllers/todos/todos_page_controller.dart';
+import '../../helpers/widgets/my_spacing.dart';
+
+class TodosPage extends StatefulWidget {
+  const TodosPage({super.key});
+
+  @override
+  _TodosPageState createState() => _TodosPageState();
+}
+
+class _TodosPageState extends State<TodosPage> {
+  late final TodosPageController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = Get.put(TodosPageController());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Layout(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Todos Page'),
+            MySpacing.height(30),
+            // TodosList widget
+            const TodosList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
