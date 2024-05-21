df48c90d6fd7794414605c175d54ba70a031d6bf
Joshua Carey <joshua@captainapp.co.uk>
2024-05-21 09:43:11 +0100

    feat(josh): example get a stream of a collection from firestore. Added generic firstore_subscreiption_services with example todo model, and subscribed to the stream in the home controller

--- apps/crew_example/lib/controllers/home/home_page_controller.dart
+++ apps/crew_example/lib/controllers/home/home_page_controller.dart
@@ -1,3 +1,4 @@
+import 'package:crew_example/helpers/services/firestore_subscription_services.dart';
 import 'package:example_package/example_package.dart';
 import 'package:get/get.dart';
 
@@ -6,6 +7,9 @@ import '../../helpers/services/auth_services.dart';
 class HomePageController extends GetxController {
   late String nameOfUser;
   late String serviceMessage;
+  
+  final FirestoreService _firestoreService = Get.find<FirestoreService>();
+
   @override
   void onInit() {
     super.onInit();
@@ -14,7 +18,32 @@ class HomePageController extends GetxController {
 
     // see main.dart for how to register services so they can be found at runtime.
     serviceMessage = Get.find<ExamplePackageService>().getHello();
+
+    // Call printFirstTodo when controller is initialized
+    printFirstTodo();
   }
 
+  Stream<List<TodoItem>> get myCollectionStream {
+    return _firestoreService.collectionStream<TodoItem>(
+      'todos',
+      (data, documentId) => TodoItem.fromFirestore(data, documentId),
+    );
+  }
+
+  // print out the first todo item in full
+  void printFirstTodo() {
+    myCollectionStream.first.then((todoItems) {
+      if (todoItems.isNotEmpty) {
+        var firstTodo = todoItems.first;
+        print('First todo item: ${firstTodo.title}');
+        print('Description: ${firstTodo.description}');
+        print('ID: ${firstTodo.id}');
+      } else {
+        print('No todo items found.');
+      }
+    }).catchError((error) {
+      print('Error fetching todo items: $error');
+    });
+  }
   // add other methods here
 }
---
+++ apps/crew_example/lib/helpers/services/firestore_subscription_services.dart
@@ -0,0 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+abstract class FirestoreService {
+  Stream<List<T>> collectionStream<T>(
+    String collectionPath,
+    T Function(Map<String, dynamic> data, String documentId) fromFirestore,
+  );
+}
+
+class FirestoreServiceImpl extends GetxService implements FirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  Stream<List<T>> collectionStream<T>(
+    String collectionPath,
+    T Function(Map<String, dynamic> data, String documentId) fromFirestore,
+  ) {
+    return _firestore.collection(collectionPath).snapshots().map((snapshot) {
+      return snapshot.docs
+          .map((doc) => fromFirestore(doc.data(), doc.id))
+          .toList();
+    });
+  }
+}
+
+// Example of how to use FirestoreService in a controller
+class MyController extends GetxController {
+  final FirestoreService _firestoreService = Get.find<FirestoreService>();
+
+  Stream<List<TodoItem>> get myCollectionStream {
+    return _firestoreService.collectionStream<TodoItem>(
+      'myCollection',
+      (data, documentId) => TodoItem.fromFirestore(data, documentId),
+    );
+  }
+}
+
+// Example model class
+class TodoItem {
+  final String id;
+  final String title;
+  final String description;
+  final bool isDone;
+
+  TodoItem({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.isDone,
+  });
+
+  factory TodoItem.fromFirestore(Map<String, dynamic> data, String documentId) {
+    return TodoItem(
+      id: documentId,
+      title: data['title'] ?? '',
+      description: data['description'] ?? '',
+      isDone: data['isDone'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'title': title,
+      'description': description,
+      'isDone': isDone,
+    };
+  }
+}
