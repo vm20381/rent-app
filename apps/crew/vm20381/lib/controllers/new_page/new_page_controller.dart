5f4f386b0969b9c45aab24f18065fe11c6ca46a4
vm20381 <vm20381@bristol.ac.uk>
2024-05-23 08:36:01 +0100

    Spring Scroll Behavior helper and controller for new page that i forgot to commit

---
+++ apps/crew/vm20381/lib/controllers/new_page/new_page_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class NewPageController extends GetxController {
+  // Add your controller logic here
+  void navigateToNewPage() {
+    Get.toNamed('/my-profile');
+  }
+}
